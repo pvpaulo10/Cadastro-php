0d29f018d2e09ec820efa3c988889e39306a33d4
Paulo Vitor de Andrade <88509409+pvpaulo10@users.noreply.github.com>
2022-04-03 23:55:19 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,8 @@
+<?php
+if ($_POST) {
+	echo "nome: ".$_POST['nome'].'<br>';
+	echo "email: ".$_POST['email'].'<br>'; 
+	echo "assunto: ".$_POST['assunto'].'<br>'; 
+	echo "mensagem: ".$_POST['mensagem'].'<br>'; 
+	}
+?>  
